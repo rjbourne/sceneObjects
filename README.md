0b2cea45c8a80a0a3a54f3c0d09e258a59d171bd
rjbourne <57537928+rjbourne@users.noreply.github.com>
2020-09-26 18:07:54 +0100

    added assimp loader, updated to 1.2.0

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ sceneObjects library for OpenGL
 ## Use
 
 simply run the Makefile then add sceneObjects.a and sceneObjects.hpp to a folder to be included in the include path
-
+ensure that the final project is linked with the assimp library -lassimp and the libassimp.dll is provided
--- sceneObjects.hpp
+++ sceneObjects.hpp
@@ -2,12 +2,15 @@
 #define SCENEOBJECTS_H_
 
 #define SO_VERSION_MAJOR 1
-#define SO_VERSION_MINOR 1
-#define SO_VERSION_REVISION 2
+#define SO_VERSION_MINOR 2
+#define SO_VERSION_REVISION 0
 
 #include "sceneObjects.hpp"
 #include <vector>
 #include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -45,7 +48,6 @@ enum SO_ShaderOptions : unsigned int {
     SO_ALPHA_ATTRIBUTE = 64,
     SO_COLOR_ATTRIBUTE = 128
 };
-
 
 // 3D Phong model lighting class
 class SO_PhongShader : public SO_Shader {
@@ -96,6 +98,64 @@ class SO_SkyboxShader : public SO_Shader {
         void setViewMatrix(glm::mat4 viewMatrix) override;
         GLuint generate(std::vector<std::string> imageFilesIn);
         void render(GLenum depthFuncReset = GL_LESS);
+};
+
+struct SO_AssimpVertex {
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 normal = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec2 texCoords = glm::vec2(0.0f, 0.0f);
+};
+
+struct SO_AssimpTexture {
+    GLuint textureId;
+    std::string path;
+};
+
+class SO_AssimpShader : public SO_Shader {
+    protected:
+        int numberLights;
+        GLint normalMatrixLoc;
+        GLint viewPositionLoc;
+    public:
+        GLuint generate(int numberLightsIn);
+        void setModelMatrix(glm::mat4 modelMatrix) override;
+        void setViewPosition(glm::vec3 viewPosition) override;
+        void setLightPosition(int index, glm::vec3 lightPosition);
+        void setLightConstant(int index, float lightConstant);
+        void setLightLinear(int index, float lightLinear);
+        void setLightQuadratic(int index, float lightQuadratic);
+        void setLightAmbient(int index, glm::vec3 lightAmbient);
+        void setLightDiffuse(int index, glm::vec3 lightDiffuse);
+        void setLightSpecular(int index, glm::vec3 lightSpecular);
+};
+
+class SO_AssimpMesh {
+        GLuint vbo;
+        GLuint vao;
+        GLuint ebo;
+    public:
+        SO_AssimpShader shader;
+        std::vector<SO_AssimpVertex> vertices;
+        std::vector<unsigned int> elements;
+        std::vector<SO_AssimpTexture> diffuseMaps;
+        SO_AssimpShader* createShader(int numberLights);
+        void draw();
+        ~SO_AssimpMesh();
+};
+
+class SO_AssimpModel {
+    public:
+        std::vector<SO_AssimpMesh> meshes;
+        std::vector<SO_AssimpTexture> globalTextures;
+        std::string directory;
+        SO_AssimpModel(std::string path, int aiOptions);
+        void loadModel(std::string path, int aiOptions);
+        void processNode(aiNode* node, const aiScene* scene);
+        SO_AssimpMesh processMesh(aiMesh* mesh, const aiScene* scene);
+        std::vector<SO_AssimpTexture> loadMaterialTextures(aiMaterial* material, aiTextureType type);
+        GLuint loadTextureFromFile(std::string path);
+        void draw();
+        void createShaders(int numberLights);
 };
 
 //A 3D camera capable of creating view/projection matricces to linked shader programs on call
