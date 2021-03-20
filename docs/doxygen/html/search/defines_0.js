9acfe4a46c7f4917d43e6c606709ffe8e21fddd0
rjbourne <57537928+rjbourne@users.noreply.github.com>
2021-03-20 19:17:51 +0000

    added sphinx support

--- docs/doxygen/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['glew_5fstatic_279',['GLEW_STATIC',['../scene_objects_8hpp.html#abcde84ea0ef5f934384e4620f092c85a',1,'sceneObjects.hpp']]]
-];
--- docs/doxygen/html/search/defines_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['so_5fversion_5fmajor_280',['SO_VERSION_MAJOR',['../scene_objects_8hpp.html#a18cdd48d82ad13af03f69b1c7d7edab8',1,'sceneObjects.hpp']]],
-  ['so_5fversion_5fminor_281',['SO_VERSION_MINOR',['../scene_objects_8hpp.html#a69e7009dfbeffcbe6f07c51237236df9',1,'sceneObjects.hpp']]],
-  ['so_5fversion_5frevision_282',['SO_VERSION_REVISION',['../scene_objects_8hpp.html#a9be5260d7042882e434d0d94dbe949e0',1,'sceneObjects.hpp']]],
-  ['stb_5fimage_5fimplementation_283',['STB_IMAGE_IMPLEMENTATION',['../_s_o___assimp_model_8cpp.html#a18372412ad2fc3ce1e3240b3cf0efe78',1,'STB_IMAGE_IMPLEMENTATION():&#160;SO_AssimpModel.cpp'],['../_s_o___skybox_shader_8cpp.html#a18372412ad2fc3ce1e3240b3cf0efe78',1,'STB_IMAGE_IMPLEMENTATION():&#160;SO_SkyboxShader.cpp']]]
-];
